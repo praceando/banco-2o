03ec87f241a1b918ef630134cb661e3f1a68917c
guibs <guilherme.barbosa@germinare.org.br>
2024-09-26 11:04:38 -0300

    ajustando script

--- PostgreSQL/script.sql
+++ PostgreSQL/script.sql
@@ -24,7 +24,7 @@ cd - FK
 CREATE TABLE genero (
     id_genero SERIAL PRIMARY KEY,
     ds_genero VARCHAR(255),
-    dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE
+    dt_atualizacao TIMESTAMP DEFAULT NOW()
 );
 
 
@@ -34,13 +34,16 @@ CREATE TABLE usuario (
     cd_genero INT,
     nm_usuario VARCHAR(255),
     ds_email VARCHAR(255),
+    ds_senha VARCHAR(255),
     is_premium BOOLEAN,
     ds_usuario TEXT,
-    dt_criacao TIMESTAMP DEFAULT CURRENT_DATE,
+    dt_criacao TIMESTAMP DEFAULT NOW(),
     dt_desativacao TIMESTAMP,
-	dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE,
+	dt_atualizacao TIMESTAMP DEFAULT NOW(),
+    cd_acesso INT,
 	
     FOREIGN KEY (cd_genero) REFERENCES genero(id_genero)
+    FOREIGN KEY (cd_acesso) REFERENCES acesso(id_acesso)
 );
 
 
@@ -57,6 +60,11 @@ CREATE TABLE anunciante (
     nr_telefone VARCHAR(14) UNIQUE)
 	INHERITS (usuario);
 
+CREATE TABLE acesso (
+    id_acesso SERIAL PRIMARY KEY,
+    nm_acesso VARCHAR(255) NOT NULL UNIQUE
+);
+
 /*
 ================================================
 *                  EVENTO                      *
@@ -68,7 +76,7 @@ CREATE TABLE local (
     nm_local VARCHAR(255),
     nr_lat DECIMAL(10, 6),   
     nr_long DECIMAL(10, 6),
-    dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE
+    dt_atualizacao TIMESTAMP DEFAULT NOW()
 );
 
 
@@ -76,7 +84,7 @@ CREATE TABLE tag (
     id_tag SERIAL PRIMARY KEY,
     nm_tag VARCHAR(255),
     ds_categoria VARCHAR(255),
-	dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE
+	dt_atualizacao TIMESTAMP DEFAULT NOW()
 );
 
 
@@ -92,7 +100,7 @@ CREATE TABLE evento (
     dt_fim TIME,
     hr_fim TIME,
     url_documentacao TEXT,
-    dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE,
+    dt_atualizacao TIMESTAMP DEFAULT NOW(),
 
     FOREIGN KEY (cd_local) REFERENCES local(id_local),
     FOREIGN KEY (cd_anunciante) REFERENCES usuario(id_usuario)
@@ -103,7 +111,7 @@ CREATE TABLE evento_tag (
     id_evento_tag SERIAL PRIMARY KEY,
     cd_tag INT,
     cd_evento INT,
-    dt_atualizao TIMESTAMP DEFAULT CURRENT_DATE,
+    dt_atualizao TIMESTAMP DEFAULT NOW(),
 
     FOREIGN KEY (cd_tag) REFERENCES tag(id_tag),
     FOREIGN KEY (cd_evento) REFERENCES evento(id_evento)
@@ -114,7 +122,7 @@ CREATE TABLE interesse (
     id_interesse SERIAL PRIMARY KEY,
     cd_consumidor INT,
     cd_evento INT,
-    dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE,
+    dt_atualizacao TIMESTAMP DEFAULT NOW(),
 
     FOREIGN KEY (cd_consumidor) REFERENCES usuario(id_usuario),
     FOREIGN KEY (cd_evento) REFERENCES evento(id_evento)
@@ -131,7 +139,7 @@ CREATE TABLE frase_sustentavel (
     id_frase SERIAL PRIMARY KEY,
 	ds_frase TEXT,
 	dt_frase TIMESTAMP,
-	dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE
+	dt_atualizacao TIMESTAMP DEFAULT NOW()
 );
 
 
@@ -144,7 +152,7 @@ CREATE TABLE produto (
     url_imagem TEXT,
     nm_categoria VARCHAR(255),
     dt_desativacao TIMESTAMP,
-	dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE
+	dt_atualizacao TIMESTAMP DEFAULT NOW()
 );
 
 
@@ -153,10 +161,10 @@ CREATE TABLE compra (
     cd_usuario INT,
     cd_produto INT,
 	cd_evento INT,
-    dt_compra TIMESTAMP DEFAULT CURRENT_DATE,
+    dt_compra TIMESTAMP DEFAULT NOW(),
     vl_total DECIMAL(10, 2),
     ds_status VARCHAR(255) DEFAULT 'Pendente',
-    dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE,
+    dt_atualizacao TIMESTAMP DEFAULT NOW(),
 
     FOREIGN KEY (cd_usuario) REFERENCES usuario(id_usuario),
     FOREIGN KEY (cd_produto) REFERENCES produto(id_produto),
@@ -168,7 +176,7 @@ CREATE TABLE pagamento (
 	id_pagamento SERIAL PRIMARY KEY,
 	cd_compra INT,
 	dt_pagamento TIMESTAMP,
-	dt_atualizacao TIMESTAMP DEFAULT CURRENT_DATE,
+	dt_atualizacao TIMESTAMP DEFAULT NOW(),
 	
 	FOREIGN KEY (cd_compra) REFERENCES compra(id_compra)
 );
