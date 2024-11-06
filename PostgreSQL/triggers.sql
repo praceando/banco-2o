c57ee1cae30d4b6edd7c7e783df80de6c7e219d0
= <leaoleitefernanda@gmail.com>
2024-11-06 07:25:52 -0300

    subindo trigger de desativar local

--- PostgreSQL/triggers.sql
+++ PostgreSQL/triggers.sql
@@ -29,19 +29,22 @@ CREATE OR REPLACE TRIGGER trg_desativar_usuario
 ================================================
 */
 
-CREATE OR REPLACE FUNCTION fnc_desativar_local() RETURNS TRIGGER AS $$
-BEGIN 
-	
-	UPDATE evento SET dt_desativacao=NOW()
-	WHERE evento.cd_local=OLD.id_local;
-	
-	RETURN OLD;
+CREATE OR REPLACE FUNCTION desativar_eventos()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.dt_desativacao IS NOT NULL THEN
+        UPDATE evento SET dt_desativacao = NEW.dt_desativacao
+        WHERE cd_local = NEW.id_local AND dt_desativacao IS NULL;
+    END IF;
+    RETURN NEW;
 END;
-$$ LANGUAGE 'plpgsql';
+$$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER trg_desativar_local
-	BEFORE UPDATE OF dt_desativacao ON local
-	FOR EACH ROW EXECUTE FUNCTION fnc_desativar_local();
+CREATE TRIGGER trg_desativar_eventos
+AFTER UPDATE OF dt_desativacao ON local
+FOR EACH ROW
+WHEN (OLD.dt_desativacao IS NULL AND NEW.dt_desativacao IS NOT NULL)
+EXECUTE FUNCTION desativar_eventos();
 
 /*
 
